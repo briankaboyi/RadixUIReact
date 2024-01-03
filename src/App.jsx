57bef81724d6bc3f135f5c9c78156d8f7af483e4
briankaboyi <briankaboyi0@gmail.com>
2024-01-03 12:19:46 +0300

    added popover,dialog,form

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Hero from "./components/Hero";
 import Cards from "./components/Cards";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="main-container">
--- src/components/Buy.jsx
+++ src/components/Buy.jsx
@@ -1,5 +1,9 @@
 import { Button, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import React from "react";
+import * as Dialog from '@radix-ui/react-dialog';
+import * as Form from '@radix-ui/react-form';
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+// import { Dialog } from "@radix-ui/react-dialog";
 
 function Buy() {
   return (
@@ -41,9 +45,59 @@ function Buy() {
             $21,000 - $40,000
           </Text>
         </Flex>
-        <Button size="3" style={{ alignSelf: "end" }}>
-          Search
-        </Button>
+        <Dialog.Root>
+          <Dialog.Trigger className="rm-padding" asChild>
+            <Button className="search-btn" size="3" style={{ alignSelf: "end" }}>
+              <MagnifyingGlassIcon />
+              Search
+            </Button>
+          </Dialog.Trigger>
+          <Dialog.Portal >
+            <Dialog.Overlay className="DialogOverlay" />
+            <Dialog.Content className="rm-padding DialogContent">
+              <Form.Root className="FormRoot" >
+                <Form.Field className="FormField" name="email">
+                  <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
+                    <Form.Label className="FormLabel">Country</Form.Label>
+                    <Form.Message className="FormMessage" match="valueMissing">
+                      Please enter a country
+                    </Form.Message>
+                    <Form.Message className="FormMessage" match="typeMismatch">
+                      Please provide a Country Name
+                    </Form.Message>
+                  </div>
+                  <Form.Control asChild>
+                    <input className="Input" type="name" required />
+                  </Form.Control>
+                </Form.Field>
+
+                <Form.Field className="FormField" name="question">
+                  <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
+                    <Form.Label className="FormLabel">State</Form.Label>
+                    <Form.Message className="FormMessage" match="valueMissing">
+                      Please enter a State
+                    </Form.Message>
+                  </div>
+                  <Form.Control asChild>
+                    <input className="Textarea" required />
+                  </Form.Control>
+                </Form.Field>
+                <Form.Submit asChild>
+                  <button className="Button" variant="solid" size={2}>
+                    Post question
+                  </button>
+                </Form.Submit>
+              </Form.Root>
+            </Dialog.Content>
+
+
+          </Dialog.Portal>
+
+
+        </Dialog.Root>
+
+
+
       </Flex>
     </Grid>
   );
