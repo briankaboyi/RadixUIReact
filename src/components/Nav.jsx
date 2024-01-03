4783d5d5233728eac32bd4172a77c123c0860652
briankaboyi <brianmkaboyi@gmail.com>
2024-01-03 19:48:59 +0300

    useref

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,37 +1,70 @@
-import { Button, Flex, Text } from '@radix-ui/themes'
-import React, { useState } from 'react'
+import { Button, Flex, Text } from "@radix-ui/themes";
+import React, { useState } from "react";
 
 export default function Nav() {
   const active = "active";
   const inactive = "b-faded";
 
   const handleClick = (e) => {
-    const parent = e.target.parentElement.children
-    const element = e.target
+    const parent = e.target.parentElement.children;
+    const element = e.target;
 
     for (let c of parent) {
       if (c.classList.contains(active)) {
-        c.classList.remove(active)
-        c.classList.add(inactive)
+        c.classList.remove(active);
+        c.classList.add(inactive);
       }
     }
-    element.classList.remove(inactive)
+    element.classList.remove(inactive);
 
-    element.classList.add(active)
-  }
+    element.classList.add(active);
+  };
+ 
   return (
-    <Flex className='nav' justify="between" align={'center'}>
+    <Flex className="nav" justify="between" align={"center"}>
       <Text className="logo">Urban</Text>
       <Flex className="nav-bar" gap="5">
-        <Text size="1" weight="bold" onClick={handleClick} className={"nav-item " + active}>Home</Text>
-        <Text size="1" weight="bold" onClick={handleClick} className={"nav-item " + inactive}>Services</Text>
-        <Text size="1" weight="bold" onClick={handleClick} className={"nav-item " + inactive}>Property</Text>
-        <Text size="1" weight="bold" onClick={handleClick} className={"nav-item " + inactive}>Contact</Text>
+        <Text
+          size="1"
+          weight="bold"
+          onClick={handleClick}
+          className={"nav-item " + active}
+        >
+          Home
+        </Text>
+        <Text
+          size="1"
+          weight="bold"
+          onClick={handleClick}
+          className={"nav-item " + inactive}
+        >
+          Services
+        </Text>
+        <Text
+          size="1"
+          weight="bold"
+          onClick={handleClick}
+          className={"nav-item " + inactive}
+        >
+          Property
+        </Text>
+        <Text
+          size="1"
+          weight="bold"
+          onClick={handleClick}
+          className={"nav-item " + inactive}
+        >
+          Contact
+        </Text>
       </Flex>
       <Flex gap="5">
-        <Button size="2" variant='outline'>Login</Button>
-        <Button size="2" variant="solid">Sign Up</Button>
+        <Button size="2" variant="outline">
+          Login
+        </Button>
+        <Button size="2" variant="solid">
+          Sign Up
+        </Button>
       </Flex>
     </Flex>
-  )
+  );
 }
